c3e04a175740a324c5b6fd41fefafc9fa7a0bd8b
Oskar Szumala <oskar@jetstreamaviation.pl>
2023-05-18 13:37:07 +0200

    feat(proxmox): Add clusters and hosts searching mechanism

--- src/ProxmoxWrapper/ProxmoxClientProvider.cs
+++ src/ProxmoxWrapper/ProxmoxClientProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Corsinvest.ProxmoxVE.Api;
 using Corsinvest.ProxmoxVE.Api.Extension;
+using Corsinvest.ProxmoxVE.Api.Shared.Models.Cluster;
 using Corsinvest.ProxmoxVE.Api.Shared.Models.Node;
 using Corsinvest.ProxmoxVE.Api.Shared.Models.Vm;
 using Jetstream.Proxmox.Sample.ProxmoxWrapper.Base;
@@ -22,7 +23,7 @@ internal class ProxmoxClientProvider : IProxmoxClientProvider
     public async Task<IEnumerable<NodeVmQemu>> GetMachinesList(string clusterName, string hostName)
     {
         // Try creating client from the list above
-        var client = await CreateClient(clusterName, hostName);
+        var client = await TryCreateClient(clusterName, hostName);
         // No active hosts in the cluster
         if (client is null)
             throw new NoActiveHostsException(clusterName);
@@ -51,26 +52,38 @@ internal class ProxmoxClientProvider : IProxmoxClientProvider
     private async Task<PveVmidItem> GetMachine(string clusterName, string hostName, int machineId)
     {
         // Order clusters so that the provided cluster is the first one
-        foreach (var cluster in _proxmoxClusters.OrderByDescending(clstr => clstr.Name == clusterName).ThenBy(clstr => clstr))
+        foreach (var cluster in _proxmoxClusters.OrderByDescending(clstr => clstr.Name == clusterName)
+                     .ThenBy(clstr => clstr))
         {
-            // 1. Try create client using specified host
-            var client = await CreateClient(cluster.Name, hostName);
-            
-            // 2. When we cannot connect to any host in the cluster -> try with another cluster
-            if (client is null)
-                continue;
+            // Order hosts so that the provided host is the first one
+            foreach (var host in cluster.Hosts.OrderByDescending(hst => hst.Name == hostName).ThenBy(hst => hst))
+            {
+                // 1. Try create client using specified host
+                var client = await CreateClient(host.Address, int.Parse(host.Port), cluster.Login, cluster.Password);
+                
+                // 2. When we cannot connect to the host in the cluster -> try with another host
+                if (client is null)
+                    continue;
+                
+                // 3. If login is successful -> try get resources within the host
+                var nodes = await client.Cluster.Resources.Get();
+                var machine = nodes.Where(node => node.ResourceType == ClusterResourceType.Vm && !node.IsTemplate)
+                    .FirstOrDefault(vm => vm.Id == machineId.ToString());
+                
+                // 4. If host does not cotain machine -> continue with another host or cluster
+                if (machine is null)
+                    continue;
 
-            // 3. Check if machine exists in the specified host
-            var result = await client.Cluster.Resources.Get();
-            
-            
-            if (result is null)
-                continue;
-
+                // 5. If we've reached this point - we have machine info and we can return data from the present host
+                return client.Nodes[machine.Node].Qemu[machineId];
+            }
         }
+
+        // This point means we didn't find the specified machine in any cluster and host - throw exception
+        throw new ResourceNotFoundException(nameof(PveVmidItem), machineId);
     }
 
-    private async Task<PveClient?> CreateClient(string clusterName, string hostName)
+    private async Task<PveClient?> TryCreateClient(string clusterName, string hostName)
     {
         var cluster = _proxmoxClusters.FirstOrDefault(cluster => cluster.Name == clusterName);
         if (cluster is null)
@@ -82,11 +95,22 @@ internal class ProxmoxClientProvider : IProxmoxClientProvider
         // Order hosts so that the first one is specified host, but in case there is any error, try login with another hosts
         foreach (var proxmoxHost in cluster.Hosts.OrderByDescending(hst => hst.Name == hostName).ThenBy(hst => hst))
         {
-            var client = new PveClient(proxmoxHost.Address);
+            var client = await CreateClient(proxmoxHost.Address, int.Parse(proxmoxHost.Port), cluster.Login,
+                cluster.Password);
 
-            if (await client.Login(cluster.Login, cluster.Password))
+            if (client is not null)
                 return client;
         }
+
+        return null;
+    }
+
+    private static async Task<PveClient?> CreateClient(string address, int port, string login, string password)
+    {
+        var client = new PveClient(address, port);
+
+        if (await client.Login(login, password))
+            return client;
 
         return null;
     }
